0740cae46744841cfb9d3dfcfee04a2a53301fd0
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-01 23:40:09 -0700

    make App a class with a constructor and state

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Nav from './components/Nav'
 
-function App() {
-  return (
-    <div className="App">
-      <div className="fixedNav">
-        <Nav />
+class App extends Component() {
+  constructor(props) {
+    super(props)
+    state = {
+      aboutActive: true,
+      stackActive: false,
+      projectsActive: false,
+      contactActive: false
+    }
+  }
+
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="fixedNav">
+          <Nav />
+        </div>
+        <div id="about" className="about container">
+          <h1>Brooke Kullberg</h1>
+        </div>
+        <div id="stack" className="stack container">
+        <h1>Stack</h1>
+        </div>
+        <div id="projects" className="project1 container">
+          <h1>Project 1</h1>
+        </div>
+        <div className="project2 container">
+        <h1>Project 2</h1>
+        </div>
+        <div className="project3 container">
+          <h1>Project 3</h1>
+        </div>
+        <div className="project4 container">
+          <h1>Project 4</h1>
+        </div>
+        <div id="contact" className="contact container">
+          <h1>Contact</h1>
+        </div>
       </div>
-      <div id="about" className="about container">
-        <h1>Brooke Kullberg</h1>
-      </div>
-      <div id="stack" className="stack container">
-      <h1>Stack</h1>
-      </div>
-      <div id="projects" className="project1 container">
-        <h1>Project 1</h1>
-      </div>
-      <div className="project2 container">
-      <h1>Project 2</h1>
-      </div>
-      <div className="project3 container">
-        <h1>Project 3</h1>
-      </div>
-      <div className="project4 container">
-        <h1>Project 4</h1>
-      </div>
-      <div id="contact" className="contact container">
-        <h1>Contact</h1>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
