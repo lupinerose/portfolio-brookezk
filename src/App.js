d7741e09e96ba9ffc4db3790b87673f5f1fad88d
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-01 22:51:57 -0700

    add divs for each section of the website and placeholder titles

--- src/App.js
+++ src/App.js
@@ -4,9 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-        {/* <h1>hello</h1> */}
-      {/* </header> */}
+      <div className="about container">
+        <h1>Brooke Kullberg</h1>
+      </div>
+      <div className="stack container">
+      <h1>Stack</h1>
+      </div>
+      <div className="project1 container">
+        <h1>Project 1</h1>
+      </div>
+      <div className="project2 container">
+      <h1>Project 2</h1>
+      </div>
+      <div className="project3 container">
+        <h1>Project 3</h1>
+      </div>
+      <div className="project4 container">
+        <h1>Project 4</h1>
+      </div>
+      <div className="contact container">
+        <h1>Contact</h1>
+      </div>
     </div>
   );
 }
