93ad11c9243f7e663bad0522cb3666d379de85d0
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-01 23:09:05 -0700

    add nav selection options that correspond to the sections of the website

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,13 @@ import React from 'react';
 export default function Nav() {
   return (
     <div>
-
+      <p>About</p>
+      <p>Stack</p>
+      <p>Project 1</p>
+      <p>Project 2</p>
+      <p>Project 3</p>
+      <p>Project 4</p>
+      <p>Contact</p>
     </div>
   )
 }
