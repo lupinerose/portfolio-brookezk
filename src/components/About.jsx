e5f9a02904c5a7e6ed1fea61f60afcd829e3df12
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-02 19:24:56 -0700

    update content in about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
     <div>
       <h3><span className="about-background">Full stack web and mobile software developer in Portland, OR.</span></h3>
       <h3><span className="about-background">Specializing in Javascript, C#.NET, and React.</span></h3>
-      <h3><span className="about-background">Thoughtful design and thorough planning makes for innovative and effective solutions.</span></h3>
+      <h3><span className="about-background">Dedicated to thorough design and planning to enable effective solutions.</span></h3>
       <p>&hearts; &hearts; &hearts; &hearts; &hearts;</p>
       <h3><span className="about-background">Language (human and computer) fanatic!</span></h3>
       <h3><span className="about-background">Cyber security enthusiast!</span></h3>
