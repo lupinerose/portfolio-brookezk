3799058ef060690cf69bb38e01b350d52f0344c2
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-02 17:22:37 -0700

    add skill sections to stack component

--- src/components/Stack.jsx
+++ src/components/Stack.jsx
@@ -4,7 +4,44 @@ export default function Stack() {
   
   return (
     <div>
-
+      <div className="stackGrid">
+        <div className="gridChild">
+          <h3>Languages</h3>
+          <ul>
+            <li>Javascript</li>
+          </ul>
+        </div>
+        <div className="gridChild">
+          <h3>Frameworks</h3>
+          <ul>
+            <li>Angular</li>
+          </ul>
+        </div>
+        <div className="gridChild">
+          <h3>Databases</h3> 
+          <ul>
+            <li>PhPMyAdmin</li>
+          </ul>
+        </div>
+        <div className="gridChild">
+          <h3>Hosting</h3> 
+          <ul>
+            <li>Firebase</li>
+          </ul>
+        </div>
+        <div className="gridChild">
+          <h3>Design/Styling</h3>
+          <ul>
+            <li>Figma</li>
+          </ul>
+        </div>
+        <div className="gridChild">
+          <h3>Work Flow</h3>
+          <ul>
+            <li>Git</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
