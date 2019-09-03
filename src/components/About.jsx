ba6fa8df1b3a62dafb28004f399cf669b8d43e79
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-02 18:32:11 -0700

    remove 'react native' from about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
   return (
     <div>
       <h3><span className="about-background">Full stack web and mobile software developer in Portland, OR.</span></h3>
-      <h3><span className="about-background">Specializing in Javascript, C#.NET, and React/React Native.</span></h3>
+      <h3><span className="about-background">Specializing in Javascript, C#.NET, and React.</span></h3>
       <h3><span className="about-background">Thoughtful design and thorough planning makes for innovative and effective solutions.</span></h3>
       <p>&hearts; &hearts; &hearts; &hearts; &hearts;</p>
       <h3><span className="about-background">Language (human and computer) fanatic!</span></h3>
