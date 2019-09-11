a11d6654200a0811d71d496e3cca879e37312f77
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-10 22:09:18 -0700

    add contact content and a class to style it

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 export default function Contact() {
   return(
     <div>
-      <div>
-
+      <div className="contactContent">
+        <p>brookekullberg@gmail.com</p>
+        <a target="_blank" href="https://www.linkedin.com/in/brooke-kullberg/">LinkedIn</a>
+        <a target="_blank" href="https://www.github.com/brookezk">Github</a>
       </div>
       <div className="footer">
         <h6>Original Design and Media by Brooke Kullberg! All rights reserved.</h6>
