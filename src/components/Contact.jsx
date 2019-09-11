b877dbf077d48d7c274ce4cc81d65a7925f0dbe5
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-10 22:32:10 -0700

    update formatting, add content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,11 +5,13 @@ export default function Contact() {
     <div>
       <div className="contactContent">
         <p>brookekullberg@gmail.com</p>
-        <a target="_blank" href="https://www.linkedin.com/in/brooke-kullberg/">LinkedIn</a>
-        <a target="_blank" href="https://www.github.com/brookezk">Github</a>
+        <p><a target="_blank" href="https://www.linkedin.com/in/brooke-kullberg/">LinkedIn</a></p>
+        <p><a target="_blank" href="https://www.github.com/brookezk">Github</a></p>
+        <p>Resume</p>
       </div>
       <div className="footer">
-        <h6>Original Design and Media by Brooke Kullberg! All rights reserved.</h6>
+        <h6><em>Original Design and Media by Brooke Kullberg!</em></h6>
+        <h6>All rights reserved.</h6>
       </div>
     </div>
   ) 
