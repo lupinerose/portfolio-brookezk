567b95105aa16540364056ce6e23eeb691209067
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-10 21:57:56 -0700

    add footer to Contact to give authorship to brooke kullberg (me)

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 export default function Contact() {
   return(
     <div>
-      
+      <div>
+
+      </div>
+      <div className="footer">
+        <h6>Original Design and Media by Brooke Kullberg! All rights reserved.</h6>
+      </div>
     </div>
   ) 
 }
