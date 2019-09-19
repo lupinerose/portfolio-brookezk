70126dcd1ef545586db2cdb00d84e747064067bc
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-18 20:34:23 -0700

    update nav for evaluation of props.active to include different project sections

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -17,13 +17,13 @@ export default function Nav(props) {
   let blueGlow = {
     textShadow: "0 0 8px blue"
   }
-
+  
   return (
     <div>
       <ul className="navAnchors">
         {props.activeLink === 'about' ? <li style={yellowGlow}><a className='bold' href="#about">About</a></li> : <li><a href="#about">About</a></li>}
         {props.activeLink === 'stack' ? <li style={blueGlow}><a className='bold' href="#stack">Stack</a></li> : <li><a href="#stack">Stack</a></li>}
-        {props.activeLink === 'projects' ? <li style={redGlow}><a className='bold' href="#projects">Projects</a></li> : <li><a href="#projects">Projects</a></li>}
+        {props.activeLink === 'project1' || props.activeLink === 'project2' || props.activeLink === 'project3' || props.activeLink === 'project4' ? <li style={redGlow}><a className='bold' href="#projects">Projects</a></li> : <li><a href="#projects">Projects</a></li>}
         {props.activeLink === 'contact' ? <li style={greenGlow}><a className='bold' href="#contact">Contact</a></li> : <li><a href="#contact">Contact</a></li>}
       </ul>
     </div>
