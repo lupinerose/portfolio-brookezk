40dec675e9d0e1f07a719f90ec0c52cb7b1bc7a9
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-18 17:36:54 -0700

    add basic content and photo to Voces section

--- src/components/Project1.jsx
+++ src/components/Project1.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import voces from '../assets/voces.png';
+
+let style = {
+  // textAlign: "left"
+}
 
 export default function Project1() {
   
   return (
     <div>
-
+      <h2 style={style}>is a journalism website that covers Latinx issues in the United States.</h2>
+      <div>
+        <div className="flex">
+          <p>HIGHTLGHTS</p>
+        </div>
+        <img src={voces} width="500px" height="auto" alt="voces website"/>
+      </div>
     </div>
   )
 }
