f310f63447c931dafc3073a5e92d73304390011d
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-18 19:01:16 -0700

    adjust boilerplate: import relevant pictures, and update the content

--- src/components/Project2.jsx
+++ src/components/Project2.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import voces from '../assets/voces.png';
+import pdx1 from '../assets/pdxfieldguide1.png';
+import pdx2 from '../assets/pdfieldguide2.png';
 
 let style = {
   h2: {
     // textAlign: "center",
+    marginTop: 20,
     marginLeft: "120px",
     fontSize: "30px"
   },
@@ -13,12 +15,12 @@ let style = {
     // backgroundColor: "white",
     paddingLeft: 20,
     paddingRight: 20,
-    textAlign: "center",
-    marginLeft: 100
+    textAlign: "center"
+    // marginLeft: 100
   },
   span: {
-    backgroundColor: "rgba(228, 180, 72)",
-    padding: 10
+    backgroundColor: "#589377",
+    padding: 5
   }
 }
 
@@ -26,15 +28,17 @@ export default function Project1() {
   
   return (
     <div>
-      <h2 style={style.h2}><span style={style.span}>A journalism website that covers Latinx issues in the United States.</span></h2>
+      <h2 style={style.h2}><span style={style.span}>Team Project! A digital field guide to Portland's flora.</span></h2>
       <div className="flex">
         <div style={style.info}>
-          <p><span className="proj1-background">&#10007; Typescript & React front-end</span></p>
-          <p><span className="proj1-background">&#10007; C# back-end</span></p>
-          <p><span className="proj1-background">&#10007; React-Router</span></p>
-          <p><span className="proj1-background">&#10007; React-Player</span></p>
+          <p><span className="proj1-background">&#10007; Website designed mobile-first</span></p>
+          <p><span className="proj1-background">&#10007; Built in Javascript & Angular, and Materialize CSS</span></p>
+          <p><span className="proj1-background">&#10007; Hosted on Firebase with their Realtime db</span></p>
+          <p><span className="proj1-background">&#10007; My work: creating the JSON database object,</span></p>
+          <p><span className="proj1-background">the methods to query the db, & the search function</span></p>
         </div>
-        <img src={voces} width="600px" height="auto" alt="voces website"/>
+        <img src={pdx1} height="400px" width="auto" alt="voces website"/>
+        <img src={pdx2} height="400px" width="auto" alt="voces website"/>
       </div>
     </div>
   )
