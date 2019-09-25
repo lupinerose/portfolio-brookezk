8a05dfe12d7d118df0201844f217f0dfb2230b3f
Brooke Kullberg <brookekullberg@gmail.com>
2019-09-25 09:55:26 -0700

    Updates

---
+++ precache-manifest.84e2f5853dbbf3f04c1b6a35fa5061b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21bcf3f07209db6709ce706612f80915",
+    "url": "/portfolio-brookezk/index.html"
+  },
+  {
+    "revision": "3ad24b0049585d19f7e7",
+    "url": "/portfolio-brookezk/static/css/main.17e3a527.chunk.css"
+  },
+  {
+    "revision": "265b2e874b7b096f1ce5",
+    "url": "/portfolio-brookezk/static/js/2.819a8ac2.chunk.js"
+  },
+  {
+    "revision": "3ad24b0049585d19f7e7",
+    "url": "/portfolio-brookezk/static/js/main.64d12050.chunk.js"
+  },
+  {
+    "revision": "b24b3ae67bee911ff31d",
+    "url": "/portfolio-brookezk/static/js/runtime~main.3ad1ee6f.js"
+  },
+  {
+    "revision": "8292509df216e6295d021dc7b7d0b186",
+    "url": "/portfolio-brookezk/static/media/Brooke-Kullberg-Resume.8292509d.pdf"
+  },
+  {
+    "revision": "7b135381ff1bdaf2dbf5ea58622c0196",
+    "url": "/portfolio-brookezk/static/media/Desktop.7b135381.jpg"
+  },
+  {
+    "revision": "55b9aeb07d4f6fdd91573f50bdc88237",
+    "url": "/portfolio-brookezk/static/media/assassin.55b9aeb0.gif"
+  },
+  {
+    "revision": "137c89c5002f40cb7647f03d2d1f5770",
+    "url": "/portfolio-brookezk/static/media/iovation.137c89c5.png"
+  },
+  {
+    "revision": "a0b18ab2c5540d19f7ad1e893272d762",
+    "url": "/portfolio-brookezk/static/media/pdfieldguide2.a0b18ab2.png"
+  },
+  {
+    "revision": "b750a22dd737d2ae2f995168268faf1a",
+    "url": "/portfolio-brookezk/static/media/pdxfieldguide1.b750a22d.png"
+  },
+  {
+    "revision": "526ade51368a43006b5c8e79dea8b7e1",
+    "url": "/portfolio-brookezk/static/media/voces.526ade51.png"
+  }
+]);
