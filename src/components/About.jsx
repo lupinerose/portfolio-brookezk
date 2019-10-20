ecc3f7960976ea1903e2bbcf48609176c149dc49
Brooke Kullberg <brookekullberg@gmail.com>
2019-10-19 23:07:28 -0700

    add in progres work to mobile design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,9 +8,9 @@ export default function About() {
       <h3><span className="about-background">Specializing in Javascript, C#.NET, and React.</span></h3>
       <h3><span className="about-background">Dedicated to the belief that thorough design and planning enables effective solutions.</span></h3>
       <p>&hearts; &hearts; &hearts; &hearts; &hearts;</p>
-      <h3><span className="about-background"><em>Travel and language fanatic!</em></span></h3>
+      <h3><span className="about-background"><em>Fan of travel and language!</em></span></h3>
       <h3><span className="about-background"><em>Cyber security enthusiast!</em></span></h3>
-      <h3><span className="about-background"><em>Data privacy advocate!</em></span></h3>
+      <h3><span className="about-background"><em>Consumer data privacy advocate!</em></span></h3>
     </div>
   )
 }
